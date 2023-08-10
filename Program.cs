9e7b1f7c22fe02689454524092f39ddc62d6118e
Craig Presti <craig.presti@microsoft.com>
2023-08-10 11:48:54 +0930

    Fix issue with case sensitivity

--- Program.cs
+++ Program.cs
@@ -1,11 +1,24 @@
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Abstractions;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Configurations;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System.Text.Json;
 
 var builtConfig = null as IConfigurationRoot;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureFunctionsWorkerDefaults(defaults =>
+    {
+        defaults.Serializer = new Azure.Core.Serialization.JsonObjectSerializer(
+            new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                PropertyNameCaseInsensitive = true
+            });
+    })
     .ConfigureAppConfiguration(configuration =>
     {
         var config = configuration.SetBasePath(Directory.GetCurrentDirectory())
@@ -16,6 +29,26 @@ var host = new HostBuilder()
     .ConfigureServices(services =>
     {
         services.AddSingleton(builtConfig.GetRequiredSection("AppConfig").Get<AppConfig>());
+
+        services.AddSingleton<IOpenApiConfigurationOptions>(_ =>
+        {
+            var options = new OpenApiConfigurationOptions()
+            {
+                Info = new OpenApiInfo()
+                {
+                    Version = "1.0.0",
+                    Title = "Websearch Plugin",
+                    Description = "This plugin is capable of searching the internet."                    
+                },
+                Servers = DefaultOpenApiConfigurationOptions.GetHostNames(),
+                OpenApiVersion = OpenApiVersionType.V3,
+                IncludeRequestingHostName = true,
+                ForceHttps = false,
+                ForceHttp = false,
+            };
+
+            return options;
+        });
     })
     .Build();
 
