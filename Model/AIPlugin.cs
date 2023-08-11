9dbcd1608340dcaf6fda366bbc3942ceed72f395
Craig Presti <craig.presti@microsoft.com>
2023-08-12 09:13:35 +0930

    Remove /api from route prefix. Fix AIPlugin properties not having the correct json names

--- Model/AIPlugin.cs
+++ Model/AIPlugin.cs
@@ -1,21 +1,33 @@
-﻿public class AIPlugin
+﻿using System.Text.Json.Serialization;
+
+public class AIPlugin
 {
+    [JsonPropertyName("schema_version")]
     public string SchemaVersion { get; set; } = "v1";
 
+    [JsonPropertyName("name_for_model")]
     public string NameForModel { get; set; } = "websearch";
 
+    [JsonPropertyName("name_for_human")]
     public string NameForHuman { get; set; } = "websearch";
 
+    [JsonPropertyName("description_for_model")]
     public string DescriptionForModel { get; set; } = "Searches the web";
 
+    [JsonPropertyName("description_for_human")]
     public string DescriptionForHuman { get; set; } = "Searches the web";
 
     public AIPluginAuth Auth { get; set; } = new AIPluginAuth { Type = "none" };
 
     public AIPluginAPI Api { get; set; } = new AIPluginAPI { Type = "openapi" };
 
+    [JsonPropertyName("contact_email")]
+    public string ContactEmail { get; set; } = string.Empty;
+
+    [JsonPropertyName("logo_url")]
     public string LogoUrl { get; set; } = string.Empty;
 
+    [JsonPropertyName("legal_info_url")]
     public string LegalInfoUrl { get; set; } = string.Empty;
 }
 
